eb85d1f07fcf63af931997b574755a652c34d335
Jody Klymak <jklymak@gmail.com>
2021-10-18 16:40:05 +0200

    trin conf.py to remove things we don't need

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,32 +15,9 @@ exclude_patterns = []
 
 html_theme = "mpl_sphinx_theme"
 html_static_path = ["./_static"]
-html_logo = "_static/logo2.svg"
 html_theme_options = {
      "native_site": "False",
      "logo_link": "https://matplotlib.org/stable",
-     "icon_links": [
-         {
-             "name": "gitter",
-             "url": "https://gitter.im/matplotlib",
-             "icon": "fab fa-gitter",
-         },
-         {
-             "name": "discourse",
-             "url": "https://discourse.matplotlib.org",
-             "icon": "fab fa-discourse",
-         },
-         {
-             "name": "GitHub",
-             "url": "https://github.com/matplotlib/matplotlib",
-             "icon": "fab fa-github-square",
-         },
-         {   "name": "twitter", 
-             "url":"https://twitter.com/matplotlib/", 
-             "icon":"fab fa-twitter-square",
-         },
-     ],
-     "navbar_center": ["mpl_nav_bar.html"],
 }
 
 html_css_files = [
